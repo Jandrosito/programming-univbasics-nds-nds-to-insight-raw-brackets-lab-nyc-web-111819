b5188390b1bcd1410bb521d6437e6e00641713a7
Edward Castillo <castilloedwarda@gmail.com>
2019-11-14 21:31:39 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,8 +4,7 @@ require 'directors_database'
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
-  #
-  #
+    
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
   }
@@ -18,5 +17,5 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+  result
 end
